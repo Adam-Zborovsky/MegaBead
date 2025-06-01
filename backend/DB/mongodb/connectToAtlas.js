46270074b02f1e31e4543485b73addc3abbcc3ea
Adam Zborovsky <96923903+Adam-Zborovsky@users.noreply.github.com>
2025-06-02 02:16:44 +0300

    Simplify MongoDB connection by removing unnecessary options from mongoose.connect

--- backend/DB/mongodb/connectToAtlas.js
+++ backend/DB/mongodb/connectToAtlas.js
@@ -4,10 +4,7 @@ require("dotenv").config();
 const connectToAtlasDb = async () => {
 	try {
 		const uri = process.env.MONGO_URI;
-		await mongoose.connect(uri, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+		await mongoose.connect(uri);
 		console.log("Connected to MongoDB Atlas");
 	} catch (error) {
 		console.error("Could not connect to MongoDB Atlas:", error);
